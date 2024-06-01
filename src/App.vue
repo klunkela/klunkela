90e8bbcde48221fd4b99dbb36d69d70c7a678f8e
klunkela <terrorklunk@mail.ru>
2024-06-01 18:17:07 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Hello from "@/components/Hello.vue";
+</script>
+
+<template>
+  <div>
+    <Hello :text="'через один год - 01.06.2025 - я буду успешным человеком'"/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+div {
+  max-width: 1280px;
+  min-height: 100vh;
+  margin: auto;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+defineProps({
+  text: String,
+})
+</script>
+
+<template>
+  <h3>{{ text }}</h3>
+</template>
+
+<style scoped lang="scss">
+h3 {
+  letter-spacing: 2px;
+  text-align: center;
+}
+</style>
